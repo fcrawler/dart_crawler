7b8dac3a3c18613e49995a5adfe1b6fe605221b6
tianye6 <tianye6@hisense.com>
2019-10-16 19:53:38 +0800

    1. add initialize variables 2. 201910161954

--- lib/variables.dart
+++ lib/variables.dart
@@ -5,6 +5,11 @@ void main() {
   // 使用关键字`var`声明一个变量
   var name = "teaphy";
 
+  var age;
+  age = 10;
+
+  print("age.type: ${age.runtimeType}");
+
   // 未初始化的变量的默认值为`null`
   int intDef;
   String strDef;
