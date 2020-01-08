1e3cf8764a216c48b768459086c6cf3ae828b2d7
tianye6 <tianye6@hisense.com>
2020-01-08 13:40:00 +0800

    1. update list ---1 2. 20200108133901

--- lib/list.dart
+++ lib/list.dart
@@ -54,7 +54,7 @@ void main() {
   var first = listProp.first; // 1
   var last = listProp.last; // 4
   var length = listProp.length; // 4
-  var reversed = listProp.reversed; // [4, 3, 2, 1]
+  Iterable reversed = listProp.reversed; // (4, 3, 2, 1)
   var isEmpty = listProp.isEmpty; // false
   var isNotEmpty = listProp.isNotEmpty; // true
   var single = listSingle.single; // A
@@ -102,7 +102,7 @@ void main() {
   // getRange
 
   var listGetRange = [1, 2, 3, 4, 5, 6];
-  var getRange = listGetRange.getRange(1, 4); // [2, 3, 4]
+  Iterable getRange = listGetRange.getRange(1, 4); // (2, 3, 4)
   print("getRange: $getRange");
 
   // indexOf
@@ -200,34 +200,34 @@ void main() {
   // setRange
 //  var listReplace2 = [10, 11, 12, 13, 14, 15, 16];
 //  listReplace.setRange(1, 4, listReplace2, 2); // [1, 12, 13, 14, 5, 6]
-  
-  
+
   print("listReplace: $listReplace");
-  
+
   // shuffle
   var listShuffle = [1, 2, 3, 4, 5, 6];
-  
+
   // 随机调整顺序，每次执行的结果可能不一样。
 //  listShuffle.shuffle(); // [6, 4, 3, 5, 2, 1]
   listShuffle.shuffle();
   print("listShuffle: $listShuffle");
-  
+
   // sort
   var listSort = ["one", "two", "three", "four"];
 //  listSort.sort(); // [four, one, three, two]
-  
-  listSort.sort((a, b) => a.length.compareTo(b.length)); // [one, two, four, three] 或 [two, one, four, three]
-  
+
+  listSort.sort((a, b) => a.length.compareTo(
+      b.length)); // [one, two, four, three] 或 [two, one, four, three]
+
   print("listSort: $listSort");
-  
+
   // subList
   var listSub = [1, 2, 3, 4, 5, 6];
   var listSub1 = listSub.sublist(1); // [2, 3, 4, 5, 6]
   var listSub2 = listSub.sublist(1, 4); // [2, 3, 4]
-  
+
   print("listSub1: $listSub1");
   print("listSub2: $listSub2");
-  
+
   // 判断元素是否存在
   // any
   var listExist = [1, 2, 3, 4, 5, 6];
@@ -235,26 +235,134 @@ void main() {
   var any2 = listExist.any((item) => item > 7); // false
   print("any1: $any1");
   print("any2: $any2");
-  
+
   // contains
   listExist.contains(3); // true
-  
+
   // every
   var every1 = listExist.every((item) => item > 2); // false
   var every2 = listExist.every((item) => item > 0); // true
   print("every1: $every1");
   print("every2: $every2");
-  
+
   // 获取元素
   var listEle = ["A", "B", "C"];
   // elementAt
   var ele = listEle.elementAt(1); // "B"
   print("ele: $ele");
-  
+
+  // firstWhere
+  var fw1 =
+      listEle.firstWhere((item) => item == "A", orElse: () => "None"); // "A"
+  var fw2 =
+      listEle.firstWhere((item) => item == "F", orElse: () => "None"); // "None"
+  print("fw1: $fw1");
+  print("fw2: $fw2");
+
+  // lastWhere
+  var listLastW = ["A1", "B1", "B2", "A2", "B4"];
+  var lw1 = listLastW.lastWhere((item) => item.startsWith("A"),
+      orElse: () => "None"); // "A2"
+  var lw2 = listLastW.lastWhere((item) => item.startsWith("F"),
+      orElse: () => "None"); // "None"
+  print("lw1: $lw1");
+  print("lw2: $lw2");
+
+  // singleWhere
+  var listSingleWhere = ["A1", "A2", "B1", "B2", "C1"];
+  var sw1 = listSingleWhere.singleWhere((ele) => ele.startsWith("C"), orElse: () => "None"); // "C1"
+  var sw2 = listSingleWhere.singleWhere((ele) => ele.startsWith("F"), orElse: () => "None"); // None
+  print("sw1: $sw1");
+  print("sw2: $sw2");
+
+  // 惰性求值
   // 扩展
-  
+
   // expand
   var listExp = [1, 2, 3];
-  var expand = listExp.expand((item) => [item, item].toList()); // [1, 1, 2, 2, 3, 3]
+  Iterable expand =
+      listExp.expand((item) => [item, item].toList()); // (1, 1, 2, 2, 3, 3)
   print("expand: $expand");
+
+  // followedBy
+  var listFb1 = [1, 2, 3];
+  var listFb2 = [-1, -2, -3];
+  Iterable followedBy = listFb1.followedBy(listFb2); // (1, 2, 3, -1, -2, -3)
+  print("followedBy: $followedBy");
+
+  // map
+  var listMap = [1, 2, 3];
+  var map = listMap.map((ele) => "A$ele"); // [A1, A2, A3]
+  print("map: $map");
+  
+  // skip
+  var listSkip = [1, 2, 3, 4];
+  Iterable skip1 = listSkip.skip(2); // (3, 4)
+  Iterable skip2 = listSkip.skip(4); // ()
+  print("skip1: $skip1");
+  print("skip2: $skip2");
+  
+  // take
+  var listTake = [1, 2, 3, 4];
+  Iterable take1 = listTake.take(2); // take1: (1, 2), length: 2
+  Iterable take2 = listTake.take(6); // take2: (1, 2, 3, 4), length: 4
+  print("take1: $take1, length: ${take1.length}");
+  print("take2: $take2, length: ${take2.length}");
+  
+  // takeWhile
+  var listTakeWhile = ["A1", "A2", "B1", "B2", "C1"];
+  var takeWhile1 = listTakeWhile.takeWhile((ele) => ele.startsWith("A")); // ["A1", "A2"]
+  var takeWhile2 = listTakeWhile.takeWhile((ele) => ele.startsWith("F")); // []
+  print("takeWhile1: $takeWhile1");
+  print("takeWhile2: $takeWhile2");
+  
+  // toList
+  var listToL = [1, 2, 3];
+  var ltl1 = listToL.toList(); // [1, 2, 3]
+  var ltl2 = listToL.toList(growable: false); // [1, 2, 3]
+  
+  print("ltl1: $ltl1");
+  print("ltl2: $ltl2");
+  
+  // toSet
+  var listSet = [1, 2, 3, 1, 2];
+  var set = listSet.toSet(); // {1, 2, 3}
+  print("set: $set");
+  
+  // where
+  var listWhere = [1, 2, 3];
+  Iterable<int> lw = listWhere.where((ele) => ele > 1); // (2, 3)
+  print("lw: $lw");
+  
+  // whereType
+  List<Object> listWt = [1, 2, "A", "B", true, false];
+  Iterable whereType = listWt.whereType<int>(); // (1, 2)
+  print("whereType: $whereType");
+  
+  // 及早求值
+
+  // fold
+  var listFold = [1, 2, 3, 4];
+  var fold = listFold.fold(0, (pre, ele) => pre + ele); // 10
+  print("fold: $fold");
+
+  //join
+  var listJoin = [1, 2, 3, 4];
+  var join = listJoin.join(","); //1,2,3,4
+  print("join: $join");
+
+  // 终端操作
+
+  // forEach
+  var listTerminal = [1, 2, 3];
+  // ele: 1
+  // ele: 2
+  // ele: 3
+  listTerminal.forEach((ele) => print("ele: $ele"));
+
+  // reduce
+  var listReduce = [1, 2, 3, 4];
+
+  var reduce = listReduce.reduce((pre, ele) => pre * ele); // 24
+  print("reduce: $reduce");
 }
